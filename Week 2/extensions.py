69c8d2c9a9ee3df33ed2dab3701a668ae79cada0
Martin Argirov <122651611+SirMarteenio@users.noreply.github.com>
2025-10-07 16:28:15 +0100

    Create extensions.py

---
+++ Week 2/extensions.py
@@ -0,0 +1,22 @@
+def main():
+    name = input("File name: ").lower().strip()
+    media_type(name)
+
+def media_type(a):
+    if a.endswith(".gif"):
+        print("image/gif")
+    elif a.endswith(".jpg") or a.endswith(".jpeg"):
+        print("image/jpeg")
+    elif a.endswith(".png"):
+        print("image/png")
+    elif a.endswith(".pdf"):
+        print("application/pdf")
+    elif a.endswith(".txt"):
+        print("text/plain")
+    elif a.endswith(".zip"):
+        print("application/zip")
+    else:
+        print("application/octet-stream")
+
+
+main()
