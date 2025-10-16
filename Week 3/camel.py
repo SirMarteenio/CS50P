1b93fd68ce4f3feb06b38e4fd1de71b43fa99054
Martin Argirov <122651611+SirMarteenio@users.noreply.github.com>
2025-10-16 18:21:35 +0100

    Add camel.py

---
+++ Week 3/camel.py
@@ -0,0 +1,15 @@
+def main():
+    camel_case = input("camelCase: ")
+    result = snakeify(camel_case)
+
+
+def snakeify(cc):
+    print("snake_case: ", end = "")
+    for char in cc:
+        if char.isupper():
+            print("_", end = "")
+        new_char = char.lower()
+        print(new_char, end = "")
+    print("\n")
+
+main()
